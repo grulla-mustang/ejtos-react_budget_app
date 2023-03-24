1d7f3c5e0525e5692b857439ac15d882886e16f1
grulla-mustang <117697231+grulla-mustang@users.noreply.github.com>
2023-03-23 22:53:31 -0400

    Update App.js
    Practice Budget: Exercise 2.7

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -21,7 +21,11 @@ const App = () => {
                         <Budget />
                     </div>
 
-                        {/* Add Remaining component here under */}        
+                        {/* Add Remaining component here under */}
+                 //Remaining component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
 
                         {/* Add ExpenseTotal component here under */}        
                        
